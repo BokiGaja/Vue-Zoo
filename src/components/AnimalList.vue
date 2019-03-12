477668bf5e235aa969c4b9c549ff5ebd6411c73f
BokiGaja <bgajic93@gmail.com>
2019-03-12 18:18:08 +0100

    Animals table added

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,36 @@
+<template>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Specie</th>
+            <th scope="col">Name</th>
+            <th scope="col">Date of birth</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="animal in animals">
+            <th scope="row">{{ animals.indexOf(animal) + 1 }}</th>
+            <td>{{ animal.specie }}</td>
+            <td>{{ animal.name }}</td>
+            <td>{{ animal.date }}</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                animals: [
+                    {specie: 'dog', name: 'Jack', date: '21.12.2010'},
+                    {specie: 'cat', name: 'Kitty', date: '22.12.2010'},
+                    {specie: 'fish', name: 'Nemo', date: '23.12.2010'},
+                    {specie: 'horse', name: 'Stud', date: '24.12.2010'},
+                    {specie: 'pig', name: 'Hans', date: '25.12.2010'}
+                ]
+            }
+        }
+    }
+</script>
